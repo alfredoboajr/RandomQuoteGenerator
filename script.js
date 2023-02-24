8d4764e63c070228707577f59dd01c5bf7e8018e
barneyzzzz <107818142+barneyzzzz@users.noreply.github.com>
2023-02-24 09:43:14 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const quoteText = document.getElementById('quote-text');
+const quoteAuthor = document.getElementById('quote-author');
+const newQuoteBtn = document.getElementById('new-quote');
+
+async function getQuote() {
+  const apiUrl = 'https://api.quotable.io/random';
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    quoteText.textContent = data.content;
+    quoteAuthor.textContent = data.author;
+  } catch (error) {
+    console.log('Error fetching quote: ', error);
+  }
+}
+
+newQuoteBtn.addEventListener('click', getQuote);
+
+getQuote();
